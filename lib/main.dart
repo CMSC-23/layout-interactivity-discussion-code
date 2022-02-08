02d4c2f2714ea59b7ae08c03a235084b5c4763ce
claizelcoubeili <clcepe@up.edu.ph>
2022-02-09 00:12:54 +0800

    Implemented the button row

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,17 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    // button section
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
         title: 'Flutter layout Demo',
         home: Scaffold(
@@ -56,7 +67,28 @@ class MyApp extends StatelessWidget {
               title: const Text('Flutter Layout Demo'),
             ),
             body: Column(
-              children: [titleSection],
+              children: [titleSection, buttonSection],
             )));
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
